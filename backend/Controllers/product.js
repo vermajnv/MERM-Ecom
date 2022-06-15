69e62d425e7c8bfb7447c2ffa13d0fdbf8d8796d
Nayan <nayanrahul.jnv@gmail.com>
2022-06-15 12:55:26 +0530

    add product count

--- backend/Controllers/product.js
+++ backend/Controllers/product.js
@@ -15,9 +15,11 @@ exports.createProduct = catchAsyncError (async (req, res, mext) => {
 // Get all products
 exports.getAllProducts = catchAsyncError (async (req, res, next) => {
     const productHelper = new ProductHelper(Product, req.query).search().filter().paginate(process.env.PAGINATION);
+    const productCount = await Product.countDocuments();
     const products = await productHelper.query;
     res.status(200).json({
         success : true,
+        count : productCount,
         products
     });
 });

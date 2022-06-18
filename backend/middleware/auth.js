f09358cffdfc008945e90ab7fe872b4994aaac59
Nayan <nayanrahul.jnv@gmail.com>
2022-06-18 09:07:35 +0530

    Update product access according to the roles

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const catchAsyncError = require('./catchAsyncError');
 const jwt = require('jsonwebtoken');
 const User = require('../Models/UserModel');
 
-const isAuthencated = catchAsyncError(async (req, res, next) => {
+exports.isAuthenticated = catchAsyncError(async (req, res, next) => {
     if (!req.cookies.token) {
         return next(new ErrorHandler("Unauthorised user", 401));
     }
@@ -12,4 +12,12 @@ const isAuthencated = catchAsyncError(async (req, res, next) => {
     next();
 });
 
-module.exports = isAuthencated;
+exports.authoriseRole = (...roles) =>{
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(new ErrorHandler("You are not authorised for this action"));
+        }
+        return next();
+    }
+};
+

35c33f5eabd897cba1626b815b2ca016d4e8bc29
Nayan <nayanrahul.jnv@gmail.com>
2022-06-20 15:47:36 +0530

    Add few more error handler required for jwt

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -3,13 +3,25 @@ const ErrorHandler = require('../utils/errorHandler');
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal server error";
-
+    console.log(err);
     // Wrong mongodb id error 
     if(err.name === 'CastError') {
         const message = `Resource not found. Invalid : ${err.path}`;
         err = new ErrorHandler(message, 500);
     }
-    if(err.code = 1100)
+    // Wrong JWT token error 
+    if(err.name === 'JsonWebTokenError') {
+        const message = `JSON web token in invalid. Try again`;
+        err = new ErrorHandler(message, 500);
+    }
+
+    // JWT expire error 
+    if(err.name === 'TokenExpiredError') {
+        const message = `JWT token has been expired. Try again`;
+        err = new ErrorHandler(message, 500);
+    }
+
+    if(err.code === 11000)
     {
         console.log(err);
         const message = `Dupplicate key error. Duplicate : ${err}`;

449d6d9181ad3fe553f452110ffd2f4a8f506dd0
Nayan <nayanrahul.jnv@gmail.com>
2022-06-23 15:16:03 +0530

    Update order status

--- backend/Controllers/order.js
+++ backend/Controllers/order.js
@@ -1,6 +1,7 @@
 const Order = require('../Models/OrderModel');
 const catchAsyncError = require('../middleware/catchAsyncError');
 const ErrorHandler = require('../utils/errorHandler');
+const Product = require('../Models/ProductModel');
 
 // Admin Routes
 
@@ -16,6 +17,34 @@ exports.getOrders = catchAsyncError(async (req, res, next) => {
     });
 });
 
+exports.updateOrder = catchAsyncError(async (req, res, next) => {
+    let order = await Order.findById(req.params.id);
+    if(!order){
+        return next(new ErrorHandler(`No record found against id : ${req.body.id}`))
+    }
+    if(order.status === 'delivered')
+    {
+        return next(new ErrorHandler("Product already delivered", 404));
+    }
+    if(req.body.status === 'delivered')
+    {
+        await updateStock(order.orderItems);
+    }
+    order.status = req.body.status;
+    order = await order.save({
+        validateBeforeSave : false,
+        new : true
+    });
+
+    res.status(200).json({
+        status : true, 
+        message : "Status updated successfully",
+        order
+    });
+});
+
+// User Routes
+
 exports.getOrder = catchAsyncError(async (req, res, next) => {
     const order = await Order.findById(req.params.id);
     if(!order)
@@ -27,6 +56,7 @@ exports.getOrder = catchAsyncError(async (req, res, next) => {
         order
     });
 });
+
 
 exports.newOrder = catchAsyncError(async (req, res, next) => {
     const {shippingInfo, orderItems, paymentInfo, itemsPrice, taxPrice, shippingPrice, totalPrice} = req.body;
@@ -48,7 +78,23 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
     });
 });
 
-exports.getAnOrder = catchAsyncError(async (req, res, next) => {
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find({user : req.user._id});
+    if(!orders) {
+        return next(new ErrorHandler("No order found", 404));
+    }
 
-})
+    res.status(200).json({
+        status : true,
+        orders
+    });
+});
 
+const updateStock = async (orderItems) =>
+{
+    orderItems.forEach(async (item) => {
+        const product = await Product.findById(item.product);
+        product.stock = product.stock - item.quantity;
+        await product.save();
+    });
+}
--- backend/Routes/orderRoute.js
+++ backend/Routes/orderRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {getOrders, getOrder, newOrder} = require('../Controllers/order');
+const {getOrders, getOrder, newOrder, updateOrder} = require('../Controllers/order');
 const {isAuthenticated, authoriseRole} = require('../middleware/auth');
 
 const Router = express.Router();
 
 Router.route('/').get(isAuthenticated, authoriseRole("admin"), getOrders);
 Router.route('/new').get(isAuthenticated, newOrder);
-Router.route('/:id').get(isAuthenticated, authoriseRole("admin"), getOrder);
+Router.route('/update/:id').put(isAuthenticated, authoriseRole("admin"), updateOrder);
+Router.route('/:id').get(isAuthenticated, getOrder);
 
 module.exports = Router;

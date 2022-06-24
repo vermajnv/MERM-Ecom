e4b325db993e95d896296e845580be74bd44d634
nayan <nayanrahul.jnv@gmail.com>
2022-06-24 17:52:00 +0530

    Create banner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import React from 'react';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
-import {BrowserRouter as Router} from "react-router-dom"
+import Home from './components/layout/Home/Home';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import WebFont from 'webfontloader';
 
 function App() {
@@ -17,6 +18,12 @@ function App() {
     return (
         <Router>
             <Header></Header>
+            <Routes>
+                <Route exact path="/" element={<Home></Home>}>
+
+                </Route>
+
+            </Routes>
             <Footer></Footer>
         </Router>
     );

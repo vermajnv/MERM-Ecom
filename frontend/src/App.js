97cc84735eccaa0939c8afb881b76208681ab417
Nayan <nayanrahul.jnv@gmail.com>
2022-06-24 14:48:18 +0530

    Add Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/layout/Header/Header';
+import {BrowserRouter as Router} from "react-router-dom"
+import WebFont from 'webfontloader';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    React.useEffect(() => {
+        WebFont.load({
+            google : {
+                families : ["Roboto", "Droid Sans", "Chilanka"]
+            }
+        })
+    }, []);
+
+    return (
+        <Router>
+            <Header></Header>
+        </Router>
+    );
 }
 
 export default App;

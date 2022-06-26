01a867d37f141996c61c94d12b3dccaa9684b43d
nayan <nayanrahul.jnv@gmail.com>
2022-06-26 08:50:53 +0530

    Add product reducer dummy set

---
+++ frontend/src/ReduxStorage/reducers/productReducer.js
@@ -0,0 +1,29 @@
+import {ALL_PRODUCT_REQUEST ,ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAIL, CLEAR_ERRORS} from '../constants/productConstant';
+const productReducer = (state = { product : []}, action) => {
+    switch (action.type) {
+        case ALL_PRODUCT_REQUEST:
+            return {
+                loading : true, 
+                products : []
+            }; 
+        case ALL_PRODUCT_SUCCESS : 
+            return {
+                loading : false,
+                products : action.payload.products
+            };
+        case ALL_PRODUCT_FAIL : 
+            return {
+                loading : false,
+                error : action.payload
+            };
+        case CLEAR_ERRORS : 
+            return {
+                ...state,
+                error : null 
+            }
+        default:
+            return state;
+    }
+};
+
+export default productReducer;

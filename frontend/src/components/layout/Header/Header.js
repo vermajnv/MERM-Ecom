27425ae16f650126a831c3d8414b371aaeab4e12
Nayan <nayanrahul.jnv@gmail.com>
2022-06-28 09:21:28 +0530

    Add custom overlay navbar

--- frontend/src/components/layout/Header/Header.js
+++ frontend/src/components/layout/Header/Header.js
@@ -1,56 +1,18 @@
 import React from 'react'
-import { ReactNavbar } from 'overlay-navbar';
-import { MdShoppingCart, MdSearch, MdPersonOutline } from "react-icons/md";
-import logo  from '../../../images/logo.png';
-
+// import { MdShoppingCart, MdSearch, MdPersonOutline } from "react-icons/md";
+// import logo  from '../../../images/logo.png';
+import NavBar from '../Navbar/Navbar';
 import './Header.css';
-
-const options = {
-  burgerColorHover: "#eb4034",
-  logo,
-  logoWidth: "20vmax",
-  navColor1: "white",
-  logoHoverSize: "10px",
-  logoHoverColor: "#eb4034",
-  link1Text: "Home",
-  link2Text: "Products",
-  link3Text: "Contact",
-  link4Text: "About",
-  link1Url: "/",
-  link2Url: "/products",
-  link3Url: "/contact",
-  link4Url: "/about",
-  link1Size: "1.3vmax",
-  link1Color: "rgba(35, 35, 35,0.8)",
-  nav1justifyContent: "flex-end",
-  nav2justifyContent: "flex-end",
-  nav3justifyContent: "flex-start",
-  nav4justifyContent: "flex-start",
-  link1ColorHover: "#eb4034",
-  link1Margin: "1vmax",
-  profileIconUrl: "/login",
-  profileIconColor: "rgba(35, 35, 35,0.8)",
-  searchIconColor: "rgba(35, 35, 35,0.8)",
-  cartIconColor: "rgba(35, 35, 35,0.8)",
-  profileIconColorHover: "#eb4034",
-  searchIconColorHover: "#eb4034",
-  cartIconColorHover: "#eb4034",
-  cartIconMargin: "1vmax",
-};
 
 export const Header = () => {
   return (
       <div className='headerMaster'>
-        <ReactNavbar 
-          {...options}
-          
-        >
-        </ReactNavbar>
-        <div className='HeaderIcons'>
+        <NavBar></NavBar>
+        {/* <div className='HeaderIcons'>
           <MdPersonOutline></MdPersonOutline>
           <MdSearch></MdSearch>
           <MdShoppingCart></MdShoppingCart>
-        </div>
+        </div> */}
       </div>
   )
 }
---
+++ frontend/src/components/layout/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import './Navbar.css';
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+    const [isToggledOverlay, setToggledOverlay] = useState(false);
+
+    function toggleOverlay()
+    {
+        setToggledOverlay(!isToggledOverlay);
+        document.body.classList.toggle('body-overflow');
+    }
+  return (
+    <div className={isToggledOverlay ? 'wrapper wrapper-overlay' : 'wrapper'}>
+        <div className='menu-btn' onClick={toggleOverlay}>
+            <FaBars></FaBars>
+        </div>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/products">Product</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar

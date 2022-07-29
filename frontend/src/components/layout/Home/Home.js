1d8fa062ab89a57ea81133a29a74840069be1b62
Nayan <nayanrahul.jnv@gmail.com>
2022-07-29 11:33:13 +0530

    add loader in homepage

--- frontend/src/components/layout/Home/Home.js
+++ frontend/src/components/layout/Home/Home.js
@@ -5,6 +5,7 @@ import MetaData from '../MetaData';
 import { getProducts } from '../../../ReduxStorage/actions/ProductAction';
 import {useDispatch, useSelector} from 'react-redux';
 import { useEffect } from 'react';
+import Loader from '../Loader/loader';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -15,22 +16,26 @@ const Home = () => {
   
   return (
     <>
-    <MetaData title="Home Page"></MetaData>
-    <div className="banner">
-        <p>Welcome to Ecommerce</p>
-        <h1>FIND AMAZING PRODUCTS BELOW</h1>
-        <a href="#featuredProduct">
-            <button>Scroll <CgMouse></CgMouse></button>
-        </a>
-    </div>
-    <h2 className='homeHeading' id="featuredProduct">Featured Products</h2>
-    <div className="container" id="container">
-      {
-        products && products.map(product => (
-          <Product product={product} key={product._id}></Product>
-        ))
-      }
-    </div>
+    {loading ? (<Loader></Loader>) : (
+      <>
+      <MetaData title="Home Page"></MetaData>
+      <div className="banner">
+          <p>Welcome to Ecommerce</p>
+          <h1>FIND AMAZING PRODUCTS BELOW</h1>
+          <a href="#featuredProduct">
+              <button>Scroll <CgMouse></CgMouse></button>
+          </a>
+      </div>
+      <h2 className='homeHeading' id="featuredProduct">Featured Products</h2>
+      <div className="container" id="container">
+        {
+          products && products.map(product => (
+            <Product product={product} key={product._id}></Product>
+          ))
+        }
+      </div>
+    </>
+    )}
     </>
   )
 }

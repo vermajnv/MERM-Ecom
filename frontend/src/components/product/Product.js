5e21fae59ae73e8d8379210d47ab099cbbf943c2
Nayan <nayanrahul.jnv@gmail.com>
2022-07-29 10:47:59 +0530

    make reviews dynmic

--- frontend/src/components/product/Product.js
+++ frontend/src/components/product/Product.js
@@ -3,26 +3,27 @@ import {Link} from 'react-router-dom';
 import ReactStars from 'react-rating-stars-component';
 import './Product.css';
 
-const options = {
-    count: 5,
-    size : window.innerWidth > 600 ? 25 : 20,
-    activeColor : "tomato",
-    edit : false,
-    value : 2.5,
-    isHalf : true,
-    color : "rgba(20, 20, 20, 0.1)"
-};
 
 const Product = ({product}) => {
+  const options = {
+      count: 5,
+      size : window.innerWidth > 600 ? 25 : 20,
+      activeColor : "tomato",
+      edit : false,
+      value : product.ratings,
+      isHalf : true,
+      color : "rgba(20, 20, 20, 0.1)"
+  };
   return (
     <Link className="productCard" to={product._id}>
         <img src={product.images[0].url} alt={product.name} />
         <p>{product.name}</p>
         <div>
           <ReactStars {...options}></ReactStars>
-          <span> (200 Reviews)</span>
+          <span> ({product.numOfReviews} Reviews)</span>
         </div>
-        <span>{product.price}</span>
+        
+        <span>{`\u20B9${product.price}`}</span>
     </Link>
   )
 }

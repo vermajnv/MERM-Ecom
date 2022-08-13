3ac1dc3f574bb13ec1f7aa8c9bc3e97de7e84b44
Nayan <nayanrahul.jnv@gmail.com>
2022-08-13 15:28:18 +0530

    Add error alert

--- frontend/src/components/product/Products.js
+++ frontend/src/components/product/Products.js
@@ -1,17 +1,23 @@
 import React, {Fragment, useEffect} from 'react'
 import Loader from '../layout/Loader/loader'
 import {useDispatch, useSelector} from 'react-redux';
-import { getProducts} from '../../ReduxStorage/actions/ProductAction';
+import { getProducts, clearErrors} from '../../ReduxStorage/actions/ProductAction';
 import ProductCard from './ProductCard';
 import './Products.css';
+import { useAlert } from '@blaumaus/react-alert';
 
 const Products = () => {
     const dispatch = useDispatch();
-
-    const {products, loading} = useSelector((state) => state.products);
+    const alert = useAlert();
+    const {products, loading, error} = useSelector((state) => state.products);
     useEffect(() => {
+        if(error)
+        {
+            alert.error(error);
+            dispatch(clearErrors());
+        }
         dispatch(getProducts());
-    }, [dispatch]);
+    }, [dispatch, error, alert]);
 
     return (
         <Fragment>
